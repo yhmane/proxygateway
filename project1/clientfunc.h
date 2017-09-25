703dca8211beca3aff6e38fb14e48da8eed6c13d
yh_Liverpool99 <yhmane@users.noreply.github.com>
2017-09-25 14:28:52 +0900

    project1

---
+++ project1/clientfunc.h
@@ -0,0 +1,87 @@
+//
+//  dbclient.h
+//  tcp
+//
+//  Created by hwangyunho on 2017. 9. 18.
+//  Copyright © 2017년 hwangyunho. All rights reserved.
+//
+
+#ifndef __HEADER_H_
+#include "header.h"
+#endif
+
+#ifndef __CLIENTFUNC_H_
+#define __CLIENTFUNC_H_
+
+void setClntmessage1(char *id, char *password, UB2 msgtype, dgt_auth_req_msg *auth_req)		//set msgtype1
+{
+    auth_req->msg_type=auth_req_msg;
+    memcpy(auth_req->user_id, id, strlen(id)+1);
+    memcpy(auth_req->password, password, strlen(id)+1);
+}
+void sendClntmessage1(int sock, dgt_auth_req_msg *auth_req)					//send msgtype1
+{
+    checklen=send(sock, auth_req, sizeof(*auth_req),0);
+	if(checklen==-1)
+		error_handling("send() error");   
+}
+
+void recvServmessage2(int sock, dgt_auth_res_msg *auth_res)					//receive msgtype2
+{
+	checklen=recv(sock, auth_res,sizeof(*auth_res),0);
+	if(checklen==-1)
+		error_handling("recv() error");
+}
+
+void setClntmessage3(UB2 msgtype, UB4 sql_len, char *sql_text, dgt_sql_req_msg *sql_req)	//set msgtype3
+{
+	int i;
+	char t[1025];
+    sql_req->msg_type=msgtype;
+    sql_req->sql_len = strlen(sql_text);
+    memcpy(sql_req->sql_text, sql_text, strlen(sql_text)+1);
+    
+    for(i =0; i<6;i++)
+        t[i]=sql_req->sql_text[i];
+    if((strcmp(t,"select") ==0) || (strcmp(t,"SELECT") ==0))
+        sql_req->sql_type=select;
+    else if((strcmp(t,"delete") ==0) || (strcmp(t,"DELETE") ==0))
+        sql_req->sql_type=delete;
+    else if((strcmp(t,"update") ==0) || (strcmp(t,"UPDATE") ==0))
+        sql_req->sql_type=update;
+    else
+        sql_req->sql_type=sqlerror;
+}
+void sendClntmessage3(int sock, dgt_sql_req_msg *sql_req)			//send msgtype3
+{
+    checklen=send(sock, sql_req, sizeof(*sql_req),0);
+	if(checklen==-1)
+		error_handling("send() error");
+}
+
+void recvServmessage4(int sock, dgt_sql_res_msg *sql_res)			//receive msgtype4
+{
+	checklen=recv(sock, sql_res,sizeof(*sql_res),0);
+	if(checklen==-1)
+		error_handling("recv() error");
+}
+
+void setClntmessage5(UB2 msgtype, dgt_close_req_msg *close_req)		//set msgtype5
+{
+	close_req->msg_type=close_req_msg;
+}
+void sendClntmessage5(int sock, dgt_close_req_msg *close_req)		//send msgtype5
+{
+	checklen=send(sock, close_req, sizeof(*close_req),0);
+	if(checklen==-1)
+		error_handling("send() error");
+}
+
+void recvServmessage6(int sock, dgt_close_res_msg *close_res)		//receive msgtype6    
+{
+	checklen=recv(sock, close_res,sizeof(*close_res),0);
+	if(checklen==-1)
+		error_handling("recv() error");
+}
+
+#endif
---
+++ project1/serverfunc.h
@@ -0,0 +1,110 @@
+//
+//  serverfunc.h
+//  tcp
+//
+//  Created by hwangyunho on 2017. 9. 18.
+//  Copyright © 2017년 hwangyunho. All rights reserved.
+//
+
+
+#ifndef __HEADER_H_
+#include "header.h"
+#endif
+
+#ifndef __SERVERFUNC_H_
+#define __SERVERFUNC_H_
+
+
+void read_childproc(int sig)
+{
+    pid_t pid;
+    int status;
+    pid=waitpid(-1, &status, WNOHANG);
+    printf("removed proc id: %d \n", pid);
+}
+void recvClntmessage1(int clnt_sock, dgt_auth_req_msg *auth_req)                   //receive ServerMessage Type2 from server
+{
+    checklen=recv(clnt_sock, auth_req,sizeof(*auth_req),0);
+    if(checklen==-1)
+        error_handling("recv() error");
+}
+
+void setServmessage2(UB2 msgtype, dgt_auth_res_msg *auth_res)
+{
+    char r2_msg[257]="================================\nConnection is Successful.\nServer IP :192.168.10.111\nServer Port : 1521\n================================\n";
+    auth_res->msg_type=auth_res_msg ;
+    auth_res->rtn_len=strlen(r2_msg);
+    memcpy(auth_res->rtn_msg, r2_msg, strlen(r2_msg)+1);
+}
+void sendServmessage2(int clnt_sock, dgt_auth_res_msg *auth_res)
+{   
+    checklen=send(clnt_sock, auth_res, sizeof(*auth_res),0);
+    if(checklen==-1)
+        error_handling("send() error");
+}
+
+void recvClntmessage3(int clnt_sock, dgt_sql_req_msg *sql_req)                 //receive ServerMessage Type2 from server
+{
+    checklen=recv(clnt_sock, sql_req, sizeof(*sql_req),0);
+    if(checklen==-1)
+        error_handling("recv() error");
+}
+
+void setServmessage4(UB2 msgtype, dgt_sql_req_msg *sql_req, dgt_sql_res_msg *sql_res)
+{
+    char sql1[]="Result : select sql is successful.\n";
+    char sql2[]="Result : delete sql is successful.\n";
+    char sql3[]="Result : update sql is successful.\n";
+    char sql4[]="Result : unsupported sql is executed.\n";
+    sql_res->msg_type=sql_res_msg;
+    sql_res->sql_type=sql_req->sql_type;
+    memset(sql_res->rtn_data, '\0', sizeof(sql_res->rtn_data));
+    
+    if(sql_res->sql_type==select) {
+        memcpy(sql_res->rtn_data, sql1, strlen(sql1));
+        sql_res->rtn_len=strlen(sql1);
+    }
+    else if(sql_res->sql_type==delete) {
+        memcpy(sql_res->rtn_data, sql2, strlen(sql2));
+        sql_res->rtn_len=strlen(sql2);
+    }
+    else if(sql_res->sql_type==update) {
+        memcpy(sql_res->rtn_data, sql3, strlen(sql3));
+        sql_res->rtn_len=strlen(sql3);
+    }
+    else {
+        memcpy(sql_res->rtn_data, sql4, strlen(sql4));
+        sql_res->rtn_len=strlen(sql4);
+    }
+}
+
+void sendServmessage4(int clnt_sock, dgt_sql_res_msg *sql_res)
+{   
+    checklen=send(clnt_sock, sql_res, sizeof(*sql_res),0);
+    if(checklen==-1)
+        error_handling("send() error");
+}
+
+void recvClntmessage5(int clnt_sock, dgt_close_req_msg *close_req)                 //receive ServerMessage Type2 from server
+{
+    checklen=recv(clnt_sock, close_req, sizeof(*close_req),0);
+    if(checklen==-1)
+        error_handling("recv() error");
+}
+
+void setServmessage6(UB2 msgtype, dgt_close_res_msg *close_res)
+{
+    char final[]="Server connection is closed\n";
+    close_res->msg_type=close_res_msg;
+    close_res->rtn_len=strlen(final);   
+    memcpy(close_res->rtn_msg, final, strlen(final));
+}
+
+void sendServmessage6(int clnt_sock, dgt_close_res_msg *close_res)
+{   
+    checklen=send(clnt_sock, close_res, sizeof(*close_res),0);
+    if(checklen==-1)
+        error_handling("send() error");
+}
+
+#endif
