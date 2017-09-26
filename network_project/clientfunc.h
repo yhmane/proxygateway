501a90cf58f6e8797abda17660f06172d4d84aea
yh_Liverpool99 <yhmane@users.noreply.github.com>
2017-09-26 16:00:09 +0900

    network

--- network_project/clientfunc.h
+++ network_project/clientfunc.h
@@ -19,12 +19,12 @@
 #define __CLIENTFUNC_H_
 
 void setClntmessage1(char *id, char *password, UB2 msgType, dgt_auth_req_msg *auth_req);		//set msgtype1
-void sendClntmessage1(int sock, dgt_auth_req_msg *auth_req);					                      //send msgtype1
-void recvServmessage2(int sock, dgt_auth_res_msg *auth_res);					                      //receive msgtype2
+void sendClntmessage1(int sock, dgt_auth_req_msg *auth_req);					       //send msgtype1
+void recvServmessage2(int sock, dgt_auth_res_msg *auth_res);					       //receive msgtype2
 void setClntmessage3(UB2 msgType, UB4 sql_len, char *sql_text, dgt_sql_req_msg *sql_req); 	//set msgtype3
-void sendClntmessage3(int sock, dgt_sql_req_msg *sql_req);					                        //send msgtype3
-void recvServmessage4(int sock, dgt_sql_res_msg *sql_res);					                        //receive msgtype4
-void setClntmessage5(UB2 msgType, dgt_close_req_msg *close_req);					                  //set msgtype5
-void sendClntmessage5(int sock, dgt_close_req_msg *close_req);					                    //send msgtype5
-void recvServmessage6(int sock, dgt_close_res_msg *close_res);					                    //receive msgtype6    
+void sendClntmessage3(int sock, dgt_sql_req_msg *sql_req);					         //send msgtype3
+void recvServmessage4(int sock, dgt_sql_res_msg *sql_res);					         //receive msgtype4
+void setClntmessage5(UB2 msgType, dgt_close_req_msg *close_req);					   //set msgtype5
+void sendClntmessage5(int sock, dgt_close_req_msg *close_req);					     //send msgtype5
+void recvServmessage6(int sock, dgt_close_res_msg *close_res);					     //receive msgtype6    
 #endif
